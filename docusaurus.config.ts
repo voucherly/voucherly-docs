59fcd34501909f2edf91108d1797a4e70424a7ff
Francesco Bognini <info@francescobognini.com>
2024-12-19 12:16:15 +0100

    Hide i18n dropdown

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -84,10 +84,10 @@ const config: Config = {
           label: 'Dashboard',
           position: 'right',
         },
-        {
-          type: 'localeDropdown',
-          position: 'right'
-        }
+        // {
+        //   type: 'localeDropdown',
+        //   position: 'right'
+        // }
       ],
     },
     footer: {
