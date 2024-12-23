84be7a40ac1313c38186a9e04ef8ce098fde8df3
Francesco Bognini <info@francescobognini.com>
2024-12-23 10:44:52 +0100

    remove footer

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -92,51 +92,6 @@ const config: Config = {
         // }
       ],
     },
-    footer: {
-      links: [],
-      // links: [
-      //   {
-      //     title: 'Docs',
-      //     items: [
-      //       {
-      //         label: 'Tutorial',
-      //         to: '/docs/intro',
-      //       },
-      //     ],
-      //   },
-      //   {
-      //     title: 'Community',
-      //     items: [
-      //       {
-      //         label: 'Stack Overflow',
-      //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-      //       },
-      //       {
-      //         label: 'Discord',
-      //         href: 'https://discordapp.com/invite/docusaurus',
-      //       },
-      //       {
-      //         label: 'X',
-      //         href: 'https://x.com/docusaurus',
-      //       },
-      //     ],
-      //   },
-      //   {
-      //     title: 'More',
-      //     items: [
-      //       {
-      //         label: 'Blog',
-      //         to: '/blog',
-      //       },
-      //       {
-      //         label: 'GitHub',
-      //         href: 'https://github.com/facebook/docusaurus',
-      //       },
-      //     ],
-      //   },
-      // ],
-      copyright: `${new Date().getFullYear()} © Voucherly`,
-    },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
