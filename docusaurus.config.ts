7c8b8aef93adf5e3533eabae0ee7e237fea3e555
Francesco Bognini <info@francescobognini.com>
2024-12-24 14:52:38 +0100

    Remove it from locales

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -31,7 +31,7 @@ const config: Config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'it'],
+    locales: ['en'],
     path: 'i18n',
     localeConfigs: {
       en: {
